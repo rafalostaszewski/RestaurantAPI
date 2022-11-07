33643cfe688514f420ff9263ba7fec57258c2c41
Rafal <ostaszewski90@gmail.com>
2022-11-07 19:17:28 +0100

    add AutoMapper and mapping profiles

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Controllers
 {
@@ -11,28 +14,36 @@ namespace RestaurantAPI.Controllers
     public class RestaurantController : ControllerBase
     {
         private readonly RestaurantDbContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public RestaurantController(RestaurantDbContext dbContext)
+        public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
             var restaurants = _dbContext
                 .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
                 .ToList();
 
-            return Ok(restaurants);
+            var restaurantsDtos = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            return Ok(restaurantsDtos);
 
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Restaurant> Get([FromRoute] int id)
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
         {
             var restaurant = _dbContext
                 .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
                 .FirstOrDefault(r => r.Id == id);
 
             if (restaurant is null)
@@ -40,7 +51,8 @@ namespace RestaurantAPI.Controllers
                 return NotFound();
             }
 
-            return Ok(restaurant);
+            var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+            return Ok(restaurantDto);
         }
     }
 
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -29,6 +29,7 @@ namespace RestaurantAPI
 			services.AddControllers();
 			services.AddDbContext<RestaurantDbContext>();
 			services.AddScoped<RestaurantSeeder>();
+			services.AddAutoMapper(this.GetType().Assembly);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
