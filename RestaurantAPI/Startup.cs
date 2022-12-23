8c75f5d579cf19519f84a0156ca8c4c0d0783855
Rafal <ostaszewski90@gmail.com>
2022-12-22 21:18:54 -0300

    add dish post controller

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -33,6 +33,7 @@ namespace RestaurantAPI
 			services.AddScoped<RestaurantSeeder>();
 			services.AddAutoMapper(this.GetType().Assembly);
 			services.AddScoped<IRestaurantService, RestaurantService>();
+			services.AddScoped<IDishService, DishService>();
 			services.AddScoped<ErrorHandlingMiddleware>();
 			services.AddScoped<RequestTimeMiddleware>();
 			services.AddSwaggerGen();
