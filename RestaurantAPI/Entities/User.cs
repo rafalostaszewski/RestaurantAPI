15595fbb15993cfff269453d8e3e84601414e5b0
Rafal <ostaszewski90@gmail.com>
2023-01-07 19:33:52 -0300

    add database migration UserAndRoleAdd

---
+++ RestaurantAPI/Entities/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
